28888343e40c3b269f0ee2136a32c6bbf2722248
VictorGus <retaow@gmail.com>
2021-01-28 23:30:13 +0300

    setup connection to mongo

--- Makefile
+++ Makefile
@@ -16,15 +16,6 @@ APP_IMAGE  ?= victor13533/mobile-backend:latest
 repl:
 	rm -rf ./cpcache && clj -A:test:nrepl -m nrepl.cmdline --middleware "[cider.nrepl/cider-middleware]"
 
-react-native:
-	cd ./mobile/MobileApp/ && react-native start --port=8088
-
-deps:
-	cd ./mobile/MobileApp/ && yarn install && npm install
-
-app:
-	cd ./mobile/MobileApp/ && react-native run-android --port=8088
-
 build:
 	clojure -A:build
 	mv target/app-1.0.0-SNAPSHOT-standalone.jar app.jar
@@ -40,12 +31,6 @@ up:
 
 down:
 	docker-compose down
-
-postgres-up:
-	docker-compose -f docker-compose.db.yaml up -d
-
-postgres-down:
-	docker-compose -f docker-compose.db.yaml down
 
 docker-build:
 	docker build -f Dockerfile -t victor13533/mobile-backend .
--- src/app/dbcore.clj
+++ src/app/dbcore.clj
@@ -1,87 +1,15 @@
 (ns app.dbcore
-  (:require [honeysql.core       :as hsql]
-            [hikari-cp.core      :as hc]
-            [clojure.java.jdbc   :as jdbc]
+  (:require [clojure.java.jdbc   :as jdbc]
             [cheshire.core       :as json]
-            [dsql.pg             :as dsql]
-            [clj-postgresql.core :as pg]
+            [monger.core         :as mg]
+            [monger.credentials  :as mg-cred]
+            [monger.collection   :as mg-col]
+            [monger.conversion   :as mg-conv]
             [app.manifest        :as m]))
 
-(def pool-config (delay (pg/pool :host     (get-in m/app-config [:db :host])
-                                 :port     (get-in m/app-config [:db :port])
-                                 :user     (get-in m/app-config [:db :user])
-                                 :password (get-in m/app-config [:db :password])
-                                 :dbname   (get-in m/app-config [:db :dbname])
-                                 :hikari {:read-only true})))
+(def db-connection (delay
+                    (let [connection (mg/connect-with-credentials "localhost" 27017 (mg-cred/create "root" "chat-db" (.toCharArray "root")))
+                          db         (mg/get-db connection "chat-db")]
+                      db)))
 
-(def test-config (delay (pg/pool :host     (get-in m/app-config [:db :host])
-                                 :port     (get-in m/app-config [:db :port])
-                                 :user     (get-in m/app-config [:db :user])
-                                 :password (get-in m/app-config [:db :password])
-                                 :dbname   "fortest"
-                                 :hikari {:read-only true})))
-
-(defn query [query ctx & [format]]
-  (let [format (or format :hsql)]
-    (case format
-      :hsql
-      (->> query hsql/format
-           (jdbc/query @ctx))
-
-      :dsql
-      (->> query dsql/format
-           (jdbc/query @ctx))
-
-      (throw (Exception. (str "Unknown format " format " supported formats :hsql :dsql"))))))
-
-(defn query-first [query ctx & [format]]
-  (let [format (or format :hsql)]
-    (case format
-      :hsql
-      (->> query hsql/format
-           (jdbc/query @ctx)
-           first)
-
-      :dsql
-      (->> query dsql/format
-           (jdbc/query @ctx)
-           first)
-
-      (throw (Exception. (str "Unknown format " format " supported formats :hsql :dsql"))))))
-
-(defn execute [query ctx & [format]]
-  (let [format (or format :hsql)]
-    (case format
-      :hsql
-      (->> query hsql/format
-           (jdbc/execute! @ctx)
-           first)
-
-      :dsql
-      (->> query dsql/format
-           (jdbc/execute! @ctx)
-           first)
-
-      (throw (Exception. (str "Unknown format " format " supported formats :hsql :dsql"))))))
-
-(comment
-
-  (query-first {:select [:*]
-                :from [[:public_user :p]]
-                :left-join [[:settings :s] [:= :s.id :p.id]]} pool-config)
-
-  (query {:select [:resource]
-          :from [:public_user]} pool-config)
-
-  (query (hsql/raw "select * from public_user p left join settings s on p.settings_id = s.id") pool-config)
-
- {:name {:family "Test"
-         :given "Test"}
-  :email "test@gmail.com"
-  :username "test"
-  :age "45"
-  :active true
-  :password "encrypted"}
-  )
-
-
+(mg-col/find-maps @db-connection "Documents")
